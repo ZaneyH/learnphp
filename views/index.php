a543d5e53f4b0a863d62ec602927275417fb5d42
ZaneyH <andreashenri35@gmail.com>
2023-09-05 11:43:02 +0300

    Hello

---
+++ views/index.php
@@ -0,0 +1,14 @@
+
+<?php include "partials/header.php";?>
+
+    <?php if($num > 10): ?>
+        <h1> Suurem</h1>
+        <?php else:  ?>
+            <h1>vaiksem </h1>
+
+        <?php endif ?>
+
+        <?php include "partials/footer.php"?>
+
+
+
