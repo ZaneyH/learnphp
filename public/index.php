baeedb7fc473551b9175958c6d469bb98abe8a1d
ZaneyH <andreashenri35@gmail.com>
2023-09-11 13:50:57 +0300

    add name space example

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
-<?php
+<?php 
+
+spl_autoload_register(function ($classname){
+var_dump($classname);
+$classname = substr($classname, 4);
+require_once __DIR__ . "/../src/$classname.php";
+});
+
+
+use App\animals\Dog;
+
+$router=new App\Router();
+var_dump(new App\animals\Dog);
+
+
+
+
+
 
 //var_dump($_SERVER);
 switch ($_SERVER["REQUEST_URI"]) {
