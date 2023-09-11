828b8ddbf276cb69bdc8760f27a907c47f5491e3
ZaneyH <andreashenri35@gmail.com>
2023-09-11 13:17:58 +0300

    easy add

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
+switch ($_SERVER["REQUEST_URI"]) {
+    case "/";
+    echo "HomePage";
+    break;
+    case"/About";
+    echo"About Page";
+    break;
+    default:
+    echo"404";
+    break;
 
+};
